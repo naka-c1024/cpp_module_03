807c329b798b7a819bb871a701d2de5d1e0cb826
naka-c1024 <ny42800824@gmail.com>
2022-04-02 12:11:05 +0900

    add: first cpp03 commit

---
+++ ex00/ClapTrap.cpp
@@ -0,0 +1,19 @@
+#include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap()
+{
+}
+ClapTrap::~ClapTrap()
+{
+}
+ClapTrap::ClapTrap(const ClapTrap &other)
+{
+	*this = other;
+}
+ClapTrap &ClapTrap::operator=(const ClapTrap &other)
+{
+	if (this != &other)
+	{
+	}
+	return *this;
+}
---
+++ ex00/ClapTrap.hpp
@@ -0,0 +1,15 @@
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
+
+class ClapTrap
+{
+public:
+	ClapTrap();
+	~ClapTrap();
+	ClapTrap(ClapTrap const &other);
+	ClapTrap &operator=(ClapTrap const &other);
+private:
+
+};
+
+#endif /* CLAPTRAP_HPP */
