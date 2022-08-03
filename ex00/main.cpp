bb9f4d79bc3a59ffb3f568db46ab5473c9ef453f
naka-c1024 <ny42800824@gmail.com>
2022-08-03 11:43:20 +0900

    出力を見やすくなるよう修正

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,4 +1,5 @@
 #include "ClapTrap.hpp"
+#include <iostream>
 
 // claptrap->ハッタリ->_attack_damageが0
 
@@ -7,20 +8,23 @@ int	main(void)
 	ClapTrap a;
 	ClapTrap b("hoge");
 	ClapTrap c("satou");
+	std::cout << std::endl;
 
 	a.attack("hanako");
 	a.beRepaired(20);
 	a.takeDamage(30);
 	a.attack("tarou");
+	std::cout << std::endl;
 
 	b.attack("jirou");
 	b.beRepaired(100);
 	b.takeDamage(120);
 	b.beRepaired(100);
+	std::cout << std::endl;
 
 	for (size_t i = 0; i < 10; i++)
 		c.attack("hehe");
 	c.attack("hehe");
-
+	std::cout << std::endl;
 	return 0;
 }
